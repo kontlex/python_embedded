b88e6a451942786f991444153e5e46b6fd4ef6a7
Shavyrin Dmitry <Dmitry.Shavyrin@paragon-software.com>
2015-05-12 15:38:22 +0300

    Moving tree for TC

---
+++ python_install.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+root_dir=$(pwd)
+src_dir=${root_dir}/src
+build_dir=${root_dir}/build
+install_dir=${root_dir}/python_bin
+
+mkdir ${src_dir}
+mkdir ${build_dir}
+mkdir ${install_dir}
+
+cd ${build_dir}
+
+#building the host components
+
+CFLAGS=-fPIC CPPFLAGS=-fPIC  ${src_dir}/configure
+CFLAGS=-fPIC CPPFLAGS=-fPIC  make python Parser/pgen
+mv python hostpython
+mv Parser/pgen Parser/hostpgen
+make distclean
+
+patch -p1 < ${src_dir}/embedded_patch/Python-2.7.2-xcompile.patch
+#arm-openwrt-linux-uclibcgnueabi-
+#arm-brcm-linux-uclibcgnueabi
+
+make distclean
+
+PATH="${TC_TOOLCHAIN_PATH}:${PATH}"
+TC_TOOLCHAIN_TRIPLET=arm-openwrt-linux-uclibcgnueabi
+C_TOOLCHAIN_ARCH_FULL=arm-brcm-linux
+
+CC=${TC_TOOLCHAIN_TRIPLET}-gcc CXX=${TC_TOOLCHAIN_TRIPLET}-g++ AR=${TC_TOOLCHAIN_TRIPLET}-ar RANLIB=${TC_TOOLCHAIN_TRIPLET}-ranlib CFLAGS=-fPIC CPPFLAGS=-fPIC ${src_dir}/configure --host=${C_TOOLCHAIN_ARCH_FULL} --build=x86_64-linux-gnu --prefix=/python
+
+CFLAGS=-fPIC CPPFLAGS=-fPIC make HOSTPYTHON=./hostpython HOSTPGEN=./Parser/hostpgen BLDSHARED="${TC_TOOLCHAIN_TRIPLET}-gcc -shared" CROSS_COMPILE=${TC_TOOLCHAIN_TRIPLET}- CROSS_COMPILE_TARGET=yes HOSTARCH=${C_TOOLCHAIN_ARCH_FULL} BUILDARCH=x86_64-linux-gnu
+
+make install HOSTPYTHON=./hostpython BLDSHARED="${TC_TOOLCHAIN_TRIPLET}-gcc -shared" CROSS_COMPILE=${TC_TOOLCHAIN_TRIPLET}- CROSS_COMPILE_TARGET=yes prefix=${install_dir}
